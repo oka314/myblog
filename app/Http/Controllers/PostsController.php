3ee2ac3f5b28a4d1076ba3e1d331b85711110747
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-02 22:03:55 +0900

    ビューを整える

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,6 +9,7 @@ class PostsController extends Controller
 {
     public function index(){
         $posts = Post::latest()->get();
+        // $posts = [];
         // dd($posts->toArray());
         // return view('posts.index',['posts'=>$posts]);
         return view('posts.index')->with('posts',$posts);
