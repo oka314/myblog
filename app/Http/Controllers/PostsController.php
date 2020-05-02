8dce92a3f806781e8f97bfcbc045ac5ac1d617ed
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-02 21:07:50 +0900

    Update PostsController.php

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index(){
+        $posts = Post::all();
+        dd($posts->toArray());
         return view('posts.index');
     }
 }
