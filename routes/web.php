e21244bbb1709248f464995a9149ae8abd556ff7
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-03 14:23:00 +0900

    投稿作成のルーティング

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PostsController@index'); 
+Route::get('/posts/create','PostsController@create');
 Route::get('/posts/{post}','PostsController@show');
