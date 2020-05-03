812973a8c3a0fa6748e519baac1b43f81050b73e
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-03 19:31:29 +0900

    コメントの保存

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillble = ['body'];
+    protected $fillable = ['body'];
 
     public function post(){
         return $this->belongsTo('App\Post');
