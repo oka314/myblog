f26e6eac8a4a2dd64fb45a4766346326e8000efa
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-03 19:23:52 +0900

    コメント機能途中

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,18 +4,31 @@
 
 @section('content')
 <h1>
-  <a href="{{url('/')}}" class="header-menu">Back</a>
+  <a href="{{ url('/') }}" class="header-menu">Back</a>
   {{ $post->title }}
 </h1>
 <p>{!! nl2br(e($post->body)) !!}</p>
+
 <h2>Comments</h2>
 <ul>
-  @forelse ($post->comments as $coment)
+  @forelse ($post->comments as $comment)
   <li>
-    {{ $comment->body}}
+    {{ $comment->body }}
   </li>
   @empty
   <li>No comments yet</li>
   @endforelse
-
+</ul>
+<form method="post" action="{{ action('CommentsController@store', $post) }}">
+  {{ csrf_field() }}
+  <p>
+    <input type="text" name="body" placeholder="enter comment" value="{{ old('body') }}">
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
+  </p>
+  <p>
+    <input type="submit" value="Add Comment">
+  </p>
+</form>
 @endsection
