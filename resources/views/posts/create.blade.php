4c5acf9f2be0fc78569a6c1d44525edeca4a324d
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-03 15:32:08 +0900

    エラー文の表示
    if文でエラーがあれば表示させるようにする。
    複数のバリテーション をかければ、複数のエラー文を拾ってくるためfirstで指定して１番最初の物だけ拾ってくるようにする

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,10 +10,17 @@
 <form method="post" action="{{ url ('/posts')}}">
   {{ csrf_field()}}
   <p>
-    <input type="text" name="title" placeholder="enter title">
+    <input type="text" name="title" placeholder="enter title" value="{{old('title')}}">
+    @if ($errors->has('title'))
+    <span class="error">{{ $errors->first('title')}}</span>
+    @endif
   </p>
   <p>
-    <textarea type="text" name="body" placeholder="enter body"></textarea>
+    <textarea  name="body" placeholder="enter body">{{ old('body') }}</textarea>
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body')}}</span>
+    @endif
+
   </p>
   <p>
     <input type="submit" value="Add">
